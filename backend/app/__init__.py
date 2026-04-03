360ed0917c5fa8de50d824be9d97c5f84ad13527
Lukasz5660 <00.chmielik@gmail.com>
2026-04-03 19:12:53 +0200

    added os import

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from app.models import db
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
     return app
